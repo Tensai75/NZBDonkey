e5843b697a38863c86185c9383429d9669848a0d
Tensai <tensai@gmx.net>
2025-08-10 08:10:20 +0200

    chore: do not use separate lists for the domains
    v1.2.2 and earlier versions will continue to work, but will no longer receive updated domains

--- src/services/lists/interceptionDoaminsList.ts
+++ src/services/lists/interceptionDoaminsList.ts
@@ -1,4 +1,4 @@
-import interceptionDomainsDefaultList from '@@/lists/interceptionDomainsList_v2.json'
+import interceptionDomainsDefaultList from '@@/lists/interceptionDomainsList.json'
 
 import { fetchAndValidateList } from './functions'
 
@@ -6,7 +6,7 @@ import { DomainSettings as InterceptionDomainsListItem, defaultDomainSettings } 
 import log from '@/services/logger/debugLogger'
 
 const interceptionDoaminsList = {
-  url: `https://raw.githubusercontent.com/${import.meta.env.WXT_REPOSITORY_NAME}/${import.meta.env.WXT_BRANCH_NAME}/lists/interceptionDomainsList_v2.json`,
+  url: `https://raw.githubusercontent.com/${import.meta.env.WXT_REPOSITORY_NAME}/${import.meta.env.WXT_BRANCH_NAME}/lists/interceptionDomainsList.json`,
   expectedVersion: 2,
   sortkey: 'domain',
   defaultList: interceptionDomainsDefaultList,
