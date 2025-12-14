016640a71e904a5919dff6dbcbd4224a12a39b7b
Tensai <tensai@gmx.net>
2025-12-14 09:11:15 +0100

    chore: refactor interception content script registration

--- src/entrypoints/background/interception/contentScriptRegistrationHandler.ts
+++ src/entrypoints/background/interception/contentScriptRegistrationHandler.ts
@@ -1,34 +1,30 @@
 import { browser } from '#imports'
-import {
-  getSettings as getInterceptionSettings,
-  Settings as InterceptionSettings,
-  watchSettings as watchInterceptionSettings,
-} from '@/services/interception'
+import { getActiveDomains, watchSettings as watchInterceptionSettings } from '@/services/interception'
 import log from '@/services/logger/debugLogger'
 
 const scriptId: string = 'interception-content-script'
 const scriptSource: string = '/content-scripts/interception.js'
 
 export default function (): void {
-  getInterceptionSettings().then((settings) => {
-    registerContentScript(settings)
-  })
-  watchInterceptionSettings((settings) => {
-    log.info('interception settings have changed')
-    registerContentScript(settings)
+  // async setup
+  log.info('Setting up interception content script registration')
+  setupInterceptionContentScriptRegistration()
+  // synchronous listener for settings changes
+  watchInterceptionSettings(() => {
+    log.info('Interception settings have changed, updating interception content script registration')
+    setupInterceptionContentScriptRegistration()
   })
 }
 
-function registerContentScript(settings: InterceptionSettings): void {
-  const domains = getActiveDomainsMatchPatternArray(settings)
-  browser.scripting.getRegisteredContentScripts().then((scripts) => {
-    const isRegistered = scripts.some((script) => script.id === scriptId)
-    if (domains.length === 0) {
-      handleNoDomains(isRegistered)
-    } else {
-      handleDomainsRegistration(isRegistered, domains)
-    }
-  })
+async function setupInterceptionContentScriptRegistration(): Promise<void> {
+  const domains = await getActiveDomainsMatchPatternArray()
+  const scripts = await browser.scripting.getRegisteredContentScripts()
+  const isRegistered = scripts.some((script) => script.id === scriptId)
+  if (domains.length === 0) {
+    handleNoDomains(isRegistered)
+  } else {
+    handleDomainsRegistration(isRegistered, domains)
+  }
 }
 
 function handleNoDomains(isRegistered: boolean): void {
@@ -46,7 +42,7 @@ function handleDomainsRegistration(isRegistered: boolean, domains: Array<string>
     updateContentScript(domains)
   } else {
     log.info('Registering the interception content script')
-    registerNewContentScript(domains)
+    registerContentScript(domains)
   }
 }
 
@@ -64,15 +60,14 @@ function updateContentScript(domains: Array<string>): void {
     .catch((e: Error) => log.error('Error while updating the registration of the interception content script', e))
 }
 
-function registerNewContentScript(domains: Array<string>): void {
+function registerContentScript(domains: Array<string>): void {
   browser.scripting
     .registerContentScripts([{ id: scriptId, js: [scriptSource], matches: domains }])
     .then(() => log.info('Registration of the interception content script completed successfully'))
     .catch((e: Error) => log.error('Error while registering the interception content script', e))
 }
 
-function getActiveDomainsMatchPatternArray(settings: InterceptionSettings): Array<string> {
-  return settings.enabled
-    ? settings.domains.filter((domain) => domain.isActive).map((domain) => `*://*.${domain.domain}/*`)
-    : []
+async function getActiveDomainsMatchPatternArray(): Promise<Array<string>> {
+  const activeDomains = await getActiveDomains()
+  return activeDomains.map((domain) => `*://*.${domain.domain}/*`)
 }
