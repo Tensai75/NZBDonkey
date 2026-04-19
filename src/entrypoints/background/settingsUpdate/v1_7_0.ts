eb278d0708484c7b0d41726b101e20b38f2f73b9
Tensai <tensai@gmx.net>
2026-04-19 07:50:34 +0200

    chore: migrate search engines settings to correct key

--- src/entrypoints/background/settingsUpdate/v1_7_0.ts
+++ src/entrypoints/background/settingsUpdate/v1_7_0.ts
@@ -1,12 +1,18 @@
 import { browser } from '#imports'
-import { saveSettings as saveInterceptionSettings } from '@/services/interception'
 import {
   defaultSettings as interceptionDefaultSettings,
   Settings as InterceptionSettings,
-} from '@/services/interception/settings'
+  saveSettings as saveInterceptionSettings,
+} from '@/services/interception'
+import {
+  saveSettings as saveSearchEngineSettings,
+  defaultSettings as searchEngineDefaultSettings,
+  Settings as SearchEnginesSettings,
+} from '@/services/searchengines'
 
 export default async function (): Promise<void> {
   await migrateInterceptionSettings()
+  await migrateSearchEngineSettings()
 }
 
 async function migrateInterceptionSettings() {
@@ -27,4 +33,19 @@ async function migrateInterceptionSettings() {
   // saving the updated settings will automatically save the domain settings separately in the storage
   // and remove the old combined setting, so we don't have to worry about cleaning up old settings
   await saveInterceptionSettings(settings)
+}
+
+async function migrateSearchEngineSettings() {
+  // load old settings manually due to the typo in the key name
+  const settings = (
+    await browser.storage.sync.get({
+      ['serachenginesSettings']: searchEngineDefaultSettings,
+    })
+  )['serachenginesSettings'] as SearchEnginesSettings
+  if (settings) {
+    // save settings with the correct key name
+    await saveSearchEngineSettings(settings)
+    // delete old settings with the typo in the key name
+    await browser.storage.sync.remove('serachenginesSettings')
+  }
 }
